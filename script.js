9013a1b72cc057203c329fee1e442c8cbf26f598
vazhukov <61968116+vazhukov@users.noreply.github.com>
2020-10-24 21:09:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 'use strict'
 
-const CRIT_FAIL = "#9f2e2e";
-const CRIT_LUCK = "#2c814e";
+const CRIT_FAIL = "#d95a5a";
+const CRIT_LUCK = "#349479";
+
+const LOG_HEADER = document.getElementById('logHeader');
+const PLACEHOLDER = document.getElementById('placeholder');
+
+const ERROR = "Error";
+const ABL = "ability";
+const MIN = "min";
+const MAX = "max";
+const SUM = "sum";
+const ROLL = "roll";
+
 
 function dieToStr(count, sides) {
   return count + "d" + sides;
@@ -10,12 +21,10 @@ function dieToStr(count, sides) {
 
 class DieGroup {
   constructor(str) {
-    let cnt$side = str.split("d").map(item => +item);
-
-    this.count = (cnt$side[0] === 0 ? 1 : cnt$side[0]);
-    this.sides = cnt$side[1];
+    let [count, sides] = str.split("d").map(item => +item);
+    this.count = (count === 0 ? 1 : count);
+    this.sides = sides;
     this.throws = [];
-
     for (let i = 0; i < this.count; ++i)
       this.throws.push(this.roll());
   }
@@ -57,37 +66,21 @@ function sumArr(array) {
 }
 
 
-function printResult(result, color, bonus) {
-  let placeholder = document.getElementById('placeholder');
-  placeholder.innerHTML = "";
-  for (let i = 0; i < result.length; ++i) {
-    let resSpan = document.createElement("span");
-    resSpan.innerHTML = (i > 0 ? " " : "") + (result[i] + bonus);
-    resSpan.style.color = color[i];
-    placeholder.insertAdjacentElement("beforeEnd", resSpan);
+function letsRoll(parsedInput) {
+  let command = parsedInput[0];
+  let diceGroups = [];
+  let sourceBonus = 0;
+  for (let i = 1; i < parsedInput.length; ++i) {
+    let idx = parsedInput[i].indexOf("d");
+    if (idx !== -1)
+      diceGroups.push(new DieGroup(parsedInput[i]));
+    else
+      sourceBonus += +parsedInput[i];
   }
-}
-
-function saveInLog(command, diceGroups, bonus) {
-  let logHeader = document.getElementById('logHeader');
-  let logItem = document.createElement("li");
-  logItem.className = "logItem";
-  logItem.innerHTML = ( command + " "
-                      + diceGroups.map(dieGroup => String(dieGroup.count + "d" + dieGroup.sides)).join(" ") + " "
-                      + (bonus != 0 ? bonus : "")
-                      + " || ");
-  logHeader.insertAdjacentElement("afterEnd", logItem);
-
-  for (let i = 0; i < diceGroups.length; ++i) {
-    logItem.innerHTML += "("
-    diceGroups[i].throws.forEach(dieThrow => {
-      let rollSpan = document.createElement("span");
-      rollSpan.innerHTML = " " + dieThrow;
-      rollSpan.style.color = diceGroups[i].getColor(dieThrow);
-      logItem.insertAdjacentElement("beforeEnd", rollSpan);
-    });
-    logItem.innerHTML += ") ";
-  }
+  if (command == ABL)
+    diceGroups.push(new DieGroup("4d6"));
+  let [resultArr, colorArr, bonus] = findResult(command, diceGroups, sourceBonus);
+  printAll(parsedInput.join(" "), diceGroups, resultArr, colorArr, bonus);
 }
 
 function findResult(command, diceGroups, bonus=0) {
@@ -95,33 +88,66 @@ function findResult(command, diceGroups, bonus=0) {
   let result;
   let color = "black";
   switch (command) {
-    case "min": case "max": case "sum":
+    case MIN: case MAX: case SUM:
       for (let i = 0; i < diceGroups.length; ++i)
         groupResults.push(diceGroups[i][command]());
   }
   switch(command) {
-    case "min": [result, color] = minArr(groupResults); break;
-    case "max": [result, color] = maxArr(groupResults); break;
-    case "sum": result = sumArr(groupResults); break;
-    case "roll":
+    case MIN: [result, color] = minArr(groupResults); break;
+    case MAX: [result, color] = maxArr(groupResults); break;
+    case SUM: result = sumArr(groupResults); break;
+    case ROLL:
       result = diceGroups[0].throws.slice(0,4);
       color = result.map(function (dieThrow) {return diceGroups[0].getColor(dieThrow);});
       break;
-    case "ability":
-      let dieGroup = new DieGroup("4d6");
-      result = dieGroup.sum() - dieGroup.min()[0];
+    case ABL:
+      result = diceGroups[0].sum() - diceGroups[0].min()[0];
       break;
     default:
-      result = "Error";
+      result = ERROR;
   }
-
   if (isNaN(+result)) bonus = "";
-  if (String(result).includes("NaN")) result = "Error";
+  if (String(result).includes("NaN")) result = ERROR;
   if (Array.isArray(result))
-    printResult(result, color, bonus);
-  else
-    printResult([result], [color], bonus);
-  saveInLog(command, diceGroups, bonus);
+    return [result, color, bonus];
+  return [[result], [color], bonus];
+}
+
+function printAll(input, diceGroups, resultArr, colorArr, bonus) {
+  PLACEHOLDER.innerHTML = "";
+  printResult(resultArr, colorArr, bonus, PLACEHOLDER);
+
+  let logItem = document.createElement("li");
+  logItem.className = "logItem";
+  logItem.innerHTML = input + " || ";
+  LOG_HEADER.insertAdjacentElement("afterend", logItem);
+  if (resultArr[0] != ERROR) {
+    printDiceGroups(diceGroups, logItem);
+    logItem.innerHTML += "|| ";
+  }
+  printResult(resultArr, colorArr, bonus, logItem);
+}
+
+function printResult(resultArr, colorArr, bonus, docElem) {
+  for (let i = 0; i < resultArr.length; ++i) {
+    let resSpan = document.createElement("span");
+    resSpan.innerHTML = (i > 0 ? " " : "") + (resultArr[i] + bonus);
+    resSpan.style.color = colorArr[i];
+    docElem.insertAdjacentElement("beforeend", resSpan);
+  }
+}
+
+function printDiceGroups(diceGroups, docElem) {
+  for (let i = 0; i < diceGroups.length; ++i) {
+    docElem.innerHTML += "("
+    diceGroups[i].throws.forEach(dieThrow => {
+      let rollSpan = document.createElement("span");
+      rollSpan.innerHTML = " " + dieThrow;
+      rollSpan.style.color = diceGroups[i].getColor(dieThrow);
+      docElem.insertAdjacentElement("beforeend", rollSpan);
+    });
+    docElem.innerHTML += ") ";
+  }
 }
 
 
@@ -131,27 +157,10 @@ let table = document.getElementById('rollTable');
 table.onclick = function(event) {
   let td = event.target.closest('td');
   if (!td || !table.contains(td)) return;
-
-  let dieGroup = new DieGroup(dieToStr(td.innerHTML, td.getAttribute("die")));
-  findResult("roll", [dieGroup]);
+  letsRoll([ROLL, dieToStr(td.innerHTML, td.getAttribute("die"))]);
 };
 
 button.onclick = function() {
-  let text = document.getElementById("console").value.split(" ");
-  let command = text[0];
-  let diceGroups = [];
-  let bonus = 0;
-  for (let i = 1; i < text.length; ++i) {
-    let idx = text[i].indexOf("d");
-    if (idx !== -1) {
-      let tmp = "";
-      if (idx === 0) tmp = "1";
-      let dieGroup = new DieGroup(tmp + text[i]);
-      diceGroups.push(dieGroup);
-    } else {
-      bonus += +text[i];
-    }
-  }
-  findResult(command, diceGroups, bonus);
+  letsRoll(document.getElementById("console").value.split(" "));
   document.getElementById("console").value = "";
 }
